e6cfb49092baf8cd32cec347d16cebf1cdc7b671
rodolfo.rocha <rodolfo.rocha@datametrica.com.br>
2017-11-08 13:18:05 -0300

    incluindo codigo de evento, ainda com erros...

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,18 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Net.Sockets;
+using Connection;
 
 namespace WpfApp1
 {
@@ -23,15 +14,18 @@ namespace WpfApp1
     {
         Client client;
 
+
         public MainWindow()
         {
             InitializeComponent();
-            client = new Client(2004, "127.0.0.1");
+            client = new Client(2004, "127.0.0.1");           
             client.StartClient();
-            ThreadStart threadStart = new ThreadStart(ReceiveDataServer);
-            Thread thread = new Thread(threadStart);
-            thread.Start();
-            //criaria aqui uma thread que ouviria o servidor enquanto o socket connected==true.E atualiza o textBox do server
+            client.ReceiveEvent += HandleReceiveEvent;                      
+        }
+
+        void HandleReceiveEvent(object sender, Receive_Args e)
+        {
+            TextBoxServerResponse.AppendText(e.Message+"\n");
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -43,9 +37,8 @@ namespace WpfApp1
         {
             String s = TextBoxClientSend.Text;
             TextBoxClientSend.Clear();
-            client.Send(s);
-            client.EndMessage();
-            TextBoxSentToServer.AppendText("você disse: " + s + "\n");
+            client.Send(s);            
+            TextBoxSentToServer.AppendText("você disse: " + s + "\n");           
 
         }
 
@@ -54,14 +47,9 @@ namespace WpfApp1
 
         }
 
-        private void ReceiveDataServer()
-        {
-            //fazer melhor.......
-            while (client.GetSocket().Connected)
-            {
-                TextBoxServerResponse.AppendText(client.GetSocketReceiveResponse());
-            }
-        }
+        
+        
     }
-    
+
+
 }
